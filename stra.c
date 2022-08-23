86b8be7283b0d480e30f4a024a42407bc866fe6d
mac <2582909040@qq.com>
2022-08-23 18:52:38 +0800

    frist version of function

--- stra.c
+++ stra.c
@@ -62,24 +62,17 @@ int Str_compare(const char s1[] , const char s2[])
 
 char *Str_search( char s1[] ,  char s2[])
 {
-    size_t s1Index = 0;
-    size_t s2Index = 0;
-    if ( Str_getLength(s2) == 0 ) 
-    {
-        return s1;
-    }
-    while (s2 != NULL )
-    {
-       while (s1 != NULL)
-       {
-          
-       }
-        
-       s2Index ++;
-    }
-    
-    
-    
+    size_t index = 0 ;
+    assert(s1 != NULL && s2 != NULL);
 
-    
+    if(Str_getLength(s2) == 0)
+        return s1;
+    while (s1[index] != '\0')
+    {
+        if (!Str_compare(&s1[index] , s2))
+        {
+            return &s1[index];
+        }
+    }
+       return NULL;
 }
